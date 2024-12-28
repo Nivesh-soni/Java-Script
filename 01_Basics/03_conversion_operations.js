16fc4182c4ad538255614e4535066fa794728d65
Nivesh soni <niveshsoni36@gmail.com>
2024-12-28 09:46:37 +0000

    conversion and operations

---
+++ 01_Basics/03_conversion_operations.js
@@ -0,0 +1,60 @@
+let score = "33aa"
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// 33 => 33
+// "33aa" => NaN (Not a Number)
+// true => 1, false => 0
+// undefined => NaN
+// null => 0
+
+let isLoogedIn = 1;
+let booleanIsLoggedin = Boolean(isLoogedIn)
+console.log(booleanIsLoggedin);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Hello" => true
+
+let someNumber = 55
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
+
+// ####################### Operations #####################
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "HEllO"
+let str2 =" NIVESH"
+let str3 = str1+str2
+console.log(str3);
+
+
+console.log("1" + 2 + 2);
+console.log("1" + "2");
+console.log(1 + 2 + "2");
+
+
+console.log(true);
+console.log(+true);
+console.log(false);
+console.log(+"");
+
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+console.log(num1)
+
+
+let gameCounter = 100
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
