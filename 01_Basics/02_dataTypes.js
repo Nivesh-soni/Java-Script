366a153dbec5f6d6fb05fbff013eea6f7be736a1
Nivesh soni <niveshsoni36@gmail.com>
2024-12-28 08:42:18 +0000

    data type

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"Use strict"; // treat all JS code as newer version
+
+// alert(3+5)  We are using node js not browser
+console.log(3+2);
+
+let name = "Nivesh Soni"
+let age = 18
+let isLoggedIn= false
+let pi = 3.14
+let symbol = '&'
+
+console.table(typeof name)
+console.table(typeof age)
+console.table(typeof isLoggedIn)
+console.table(typeof pi)
+console.table(typeof symbol)
