b443f3e8bd3e8e2b2445cf6f74b3953136e12b79
Nivesh soni <niveshsoni36@gmail.com>
2024-12-28 08:12:15 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "niveshsoni@google.com"
+var accountPassword = "12345" //we don't use it
+accountCity = "Ratlam"
+let  accountDate;``
+
+//accountId = 2  not allowed
+accountEmail = "hello@js.com"
+accountPassword = "4444"
+accountCity = "jj"
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountDate])
