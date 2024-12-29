fdda02a8aacc934d5510a2e758df4881e92bed7d
Nivesh soni <niveshsoni36@gmail.com>
2024-12-29 07:07:30 +0000

    string

---
+++ 01_Basics/06_string.js
@@ -0,0 +1,5 @@
+const name = "Nivesh"
+const repoCount = 1
+// console.log(name + repoCount + y.toUpperCase() ); we don't use this type of syntax
+
+console.log(`Hello !! My name is ${name}. My repoCount is ${repoCount}`)
