6536d4204c4a8f6ca125c5de2516c149117dd467
Nivesh soni <niveshsoni36@gmail.com>
2025-01-08 15:28:17 +0000

    for

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -11,11 +11,40 @@
 // }
 
 
-for (let i = 1; i <= 10; i++) 
+// for (let i = 1; i <= 10; i++) 
+// {
+//     console.log(`Outer loop ${i}`)
+//     for(let j = 1; j <= 10; j++)
+//     {
+//         console.log(`${i} * ${j} = ${i*j}`)
+//     }
+// }
+
+let myArray = ["Flash", "Batman", "SuperMan", "Krish"]
+console.log(myArray.length)
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];  
+    console.log(element)
+}
+
+
+// Break and Continue
+
+// for(i=1; i<21; i++)
+// {
+//     if(i == 5)
+//         {
+//             console.log("5 detected")
+//             break
+//         }
+//     console.log(i)
+// }
+for(i=1; i<21; i++)
 {
-    console.log(`Outer loop ${i}`)
-    for(let j = 1; j <= 10; j++)
-    {
-        console.log(`${i} * ${j} = ${i*j}`)
-    }
+    if(i == 5)
+        {
+            console.log("5 detected")
+            continue
+        }
+    console.log(i)
 }
