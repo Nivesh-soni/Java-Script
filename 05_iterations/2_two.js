cba3c20b35a1bf0c7d7aafd1d01a2d54d33c4df3
Nivesh soni <niveshsoni36@gmail.com>
2025-01-08 15:46:17 +0000

    while and do while

---
+++ 05_iterations/2_two.js
@@ -0,0 +1,22 @@
+// While
+
+let i = 0
+while(i <= 10)
+{
+    console.log(`value of i is ${i}`)
+    i += 2
+}
+
+let myArray = ["Flash", "Batman", "SuperMan", "Krish"]
+let j = 0
+while(j < myArray.length)
+{
+    console.log(`Elements is ${myArray[j]}`)
+    j++ // j = j + 1
+}
+
+let score = 11
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score <=10);
