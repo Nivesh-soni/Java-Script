b408f65d0764cd39f26bf8a9142e6fd71b2c054c
Nivesh soni <niveshsoni36@gmail.com>
2025-01-08 05:15:19 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,21 @@
+// for loop
+
+// for(let i=0 ; i <= 10 ; i++)
+// {
+//     const element = i
+//     if(element==5)
+//     {
+//         console.log(`5 is a best number`)
+//     }
+//     console.log(element)
+// }
+
+
+for (let i = 1; i <= 10; i++) 
+{
+    console.log(`Outer loop ${i}`)
+    for(let j = 1; j <= 10; j++)
+    {
+        console.log(`${i} * ${j} = ${i*j}`)
+    }
+}
